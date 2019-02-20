ebffda3997064d0ac47479aa0cc58a64672ca864
Nirma Perera <nirmaperera23@gmail.com>
2019-02-20 18:24:45 -0500

    added files for Heroku

--- app.py
+++ app.py
@@ -137,5 +137,6 @@ def logout():
 
 
 # run the application
-if __name__ == "__app__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
